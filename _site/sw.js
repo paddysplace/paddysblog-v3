46361bba7134098ed1285b75b26295f232f16a92
Patrick <paddee28@gmail.com>
2021-07-03 06:05:53 +0100

    feed

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20210702230309';
+const version = '20210702231332';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/personal/2021/07/01/the-testing-is-over/","/test/2021/06/30/lets-try-again/","/test/2021/06/27/first-test/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/search/","/assets/styles.css","/thanks/","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/styles.css.map","https://www.freeiconspng.com/uploads/black-cat-png-22.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/personal/2021/07/01/the-testing-is-over/","/test/2021/06/30/lets-try-again/","/test/2021/06/27/first-test/","/categories/","/feed.xml","/blog/","/","/manifest.json","/assets/search.json","/search/","/assets/styles.css","/thanks/","/redirects.json","/sitemap.xml","/robots.txt","/assets/styles.css.map","https://www.freeiconspng.com/uploads/black-cat-png-22.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
